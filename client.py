19121035f58b212f2d1aea2bbe6d079e8d44431a
sequeto <joaopedrosequeto@gmail.com>
2022-08-13 14:24:19 -0300

    Adicionando Leitura de arquivo para Envio e Avaliação final

--- client.py
+++ client.py
@@ -1,12 +1,16 @@
+from cgi import test
 import socket
+import os
 
 IP_SERVIDOR = '127.0.0.1'
 PORTA_SERVIDOR = 5000
 DESTINO = (IP_SERVIDOR, PORTA_SERVIDOR)
-BUFFER_SIZE = 1024
+BUFFER_SIZE = 1
 
 udpClient = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
+tamanho_arquivo = os.path.getsize("teste.txt")
+print("tamanho do arquivo em Bytes:", tamanho_arquivo)
 mensagem = input()
 
 udpClient.sendto(str.encode(mensagem), DESTINO)
