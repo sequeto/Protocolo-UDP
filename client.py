2aba4ecbcc60be15a3aa5b555076f9983fa6e729
sequeto <joaopedrosequeto@gmail.com>
2022-08-14 19:54:49 -0300

    Adicionando Janela Deslizante

--- client.py
+++ client.py
@@ -1,14 +1,18 @@
 import socket
 import os
 import utils
+import time
+
+def sendData(packages, wnd_start, wnd_finish, udpClient, destiny):
+    for data in packages[wnd_start: wnd_finish + 1]:
+        udpClient.sendto(data['data'], destiny)
 
 # Constantes
 IP_SERVIDOR = '127.0.0.1'
 PORTA_SERVIDOR = 5000
 DESTINO = (IP_SERVIDOR, PORTA_SERVIDOR)
 
-MSS = 16
-
+MSS = 1
 
 # Inicializando UDP Socket
 udpClient = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -21,34 +25,57 @@ print("Quantidade de Pacotes: ", len(packages))
 print(packages)
 
 # Realizando Comunicação
-index = 0
+# Numero de Sequencia baseado em Inteiros
+
+send_window_start = 0 # Variáveis de Controle da Janela Deslizante
+send_window_finish = 4 # Variáveis de Controle da Janela Deslizante
+
 finished = False
+acks_received = []
+
+payload = b''
+
+# Enviando primeiro conjunto de Pacotes
+print("Enviando 5 Pacote de Tamanho: " ,  MSS)
+sendData(packages, send_window_start, send_window_finish, udpClient, DESTINO)
 
 while(not finished):
-    print("Enviando Pacote de Tamanho: " ,  MSS)
-    udpClient.sendto(packages[index]['data'], DESTINO)
 
     msgFromServer = udpClient.recvfrom(1024)[0];
-
-    if(index == (len(packages) - 1)):
-        if(int(msgFromServer.decode("utf8")) == packages[index]["seq_number"]):
-            print("ACK: ", msgFromServer)
-            finished = True;
-        else:
-            # print(msgFromServer.decode("utf8"))
-            # print('tamanho: ',len(packages[index]))
-            print("Ocorreu Perda dos Dados")
-            print("reenviando Pacote: ", packages[index]["seq_number"])
-    elif(int(msgFromServer.decode("utf8")) == packages[index]["seq_number"]):
+    if(int(msgFromServer.decode("utf8")) == packages[send_window_start]["seq_number"]):
         print("ACK: ", msgFromServer)
-        index = index + 1;
-    else:
-        # print(msgFromServer.decode("utf8"))
-        print("Ocorreu Perda dos Dados")
-        print("reenviando Pacote: ", packages[index]["seq_number"])
+        send_window_start = send_window_start + 1;
+        
+        if(send_window_finish < len(packages) - 1):
+            send_window_finish = send_window_finish + 1;
+            udpClient.sendto(packages[send_window_finish]["data"], DESTINO)
+    
+    elif(int(msgFromServer.decode("utf8")) < packages[send_window_start]["seq_number"]):
+        print("ACK: ", msgFromServer)
+        sendData(packages, send_window_start, send_window_finish, udpClient, DESTINO)
+    
+    # elif()
 
-if(finished ):
-    udpClient.sendto("FIN".encode("utf8"), DESTINO)
+#     if(index == (len(packages) - 1)):
+#         if(int(msgFromServer.decode("utf8")) == packages[index]["seq_number"]):
+#             print("ACK: ", msgFromServer)
+#             finished = True;
+#         else:
+#             # print(msgFromServer.decode("utf8"))
+#             # print('tamanho: ',len(packages[index]))
+#             print("Ocorreu Perda dos Dados")
+#             print("reenviando Pacote: ", packages[index]["seq_number"])
+#     elif(int(msgFromServer.decode("utf8")) == packages[index]["seq_number"]):
+#         print("ACK: ", msgFromServer)
+#         index = index + 1;
+#     else:
+#         # print(msgFromServer.decode("utf8"))
+#         print("Ocorreu Perda dos Dados")
+#         print("reenviando Pacote: ", packages[index]["seq_number"])
+
+# # Validar se existe desconexão
+# if(finished ):
+#     udpClient.sendto("FIN".encode("utf8"), DESTINO)
 
 
 
--- utils.py
+++ utils.py
@@ -1,21 +1,3 @@
-# def breakChunks(file_path,BUFFER):
-#     packages = []
-#     sequence_number = 1
-
-#     with open(file_path, "rb") as f:
-#         while True:
-#             data = f.read(BUFFER)
-#             if not data: break
-#             packages.append({
-#                 "data": data,
-#                 "seq_number": sequence_number
-#             })
-#             sequence_number = sequence_number + (len(data));
-#     # print(packages)
-
-#     return packages
-
-
 def breakChunks(file_path,BUFFER):
     packages = []
     sequence_number = 0
@@ -28,7 +10,7 @@ def breakChunks(file_path,BUFFER):
                 "data": data,
                 "seq_number": sequence_number
             })
+            # sequence_number = sequence_number + (len(data));
             sequence_number = sequence_number + 1;
-    # print(packages)
 
     return packages
